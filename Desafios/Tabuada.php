3eed2ba27b91f14d527e3d0a35c1e7eba3cca158
LuanCarlosCar <luancarloscar@gmail.com>
2021-06-16 01:11:26 -0300

    Novos desafios

---
+++ Desafios/Tabuada.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+//Tabuada
+$numero = 10;
+$multicador = 1;
+while($multiplicador<=10){
+    $resultado = $numero*$ultiplicador;
+    echo "$numero X $multiplicador = $resultado" .PHP_EOL;
+
+    $multicador= $multicador++;
+
+
+}
