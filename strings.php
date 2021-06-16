adb3ea9af814aba88005df6b0dc1732299499bf1
LuanCarlosCar <luancarloscar@gmail.com>
2021-06-15 23:39:08 -0300

    Update strings.php

---
+++ strings.php
@@ -0,0 +1,14 @@
+<?php 
+
+$idade = 20;
+echo 'Sou o luan, tenho' . $idade . 'anos'; // estou concatenando minha string e minha variavel atravez do '.'
+echo "Sou o luan, tenho $idade anos"; // Desta forma o php consegue ler minha variavel mesmo quando eu tiver declarando minha string
+
+echo "Ola mundo\n"; // "\n"(new line) esta quebrando a linha/ gerando uma nova linha 
+echo '\n'; // estou informando que "\n" e apenas uma string
+echo "\t"; // estou gerando um 'tab'(espaço no textto)
+echo "Sou o luan, tenho $idade anos";
+
+
+
+// mais exemplos de caracteres especiais : https://www.php.net/manual/pt_BR/language.types.string.php
