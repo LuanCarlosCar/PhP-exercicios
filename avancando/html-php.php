e49fe89a8b82254d75c929d601d6b41c65aaf480
LuanCarlosCar <luancarloscar@gmail.com>
2021-06-19 18:19:01 -0300

    finalizando curso

---
+++ avancando/html-php.php
@@ -0,0 +1,14 @@
+<body>
+    <h1>Contas correntes</h1>
+
+    <dl>
+        <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
